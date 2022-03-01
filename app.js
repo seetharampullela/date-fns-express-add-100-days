f6bda634ef540601b11ab5bd7f1859342140178a
root <seetharampullela@gmail.com>
2022-03-01 09:30:43 +0000

    Solution for date after 100days

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const addDays = require("date-fns/addDays");
+
+app.get("/", (request, response) => {
+  let date = new Date();
+  let newdate = addDays(date, 100);
+  response.send(
+    `${newdate.getDate()}/${newdate.getMonth() + 1}/${newdate.getFullYear()}`
+  );
+});
+module.exports = app;
